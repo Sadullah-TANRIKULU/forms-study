5391a5208d5e676b06f5eb1e7e866f48e803e371
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-07-01 10:57:57 +0300

    forms-study-in-class

---
+++ src/components/mouseEvents/MouseEvent.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { FaReact } from "react-icons/fa";
+import { FaYinYang } from "react-icons/fa";
+import { FaEvernote } from "react-icons/fa";
+
+const MouseEvent = () => {
+  // Mouseover olayı, fare işaretçisi div öğesine ve onun alt öğelerine girdiğinde tetiklenir. Mouseenter olayı, yalnızca fare işaretçisi div öğesine girdiğinde tetiklenir. onmousemove olayı, fare işaretçisi div öğesinin üzerine her taşındığında tetiklenir.
+
+  const [visible, setVisible] = useState(false);
+  const [toggleBG, setToggleBG] = useState(false);
+
+  const [coordX, setCoordX] = useState();
+  const [coordY, setCoordY] = useState();
+
+  const handleDoubleClick = (e) => {
+    setToggleBG(!toggleBG);
+    toggleBG
+      ? (e.target.className = "bg-danger text-light w-50 p-4 mt-4")
+      : (e.target.className = "bg-success text-light w-50 p-4 mt-4");
+  };
+
+
+  const handleMouseMove = (e) => {
+    //! Goreceli koordinatlar
+    setCoordX(e.nativeEvent.offsetX);
+    setCoordY(e.nativeEvent.offsetY);
+
+    //! Sayfaya göre koordinatlar
+    // setCoordX(e.pageX);
+    // setCoordY(e.pageY);
+  };
+
+  return (
+    <div className="container text-center d-flex flex-column align-items-center mt-4">
+      <h2 className="text-danger">MOUSE EVENTS</h2>
+
+      <div
+        id="todo-1"
+        className="bg-success text-light w-50 p-4"
+        onMouseLeave={() => setVisible(false)}
+        onMouseEnter={() => setVisible(true)}
+      >
+        todo item 1 <FaReact /> {visible && <span>when mouse enter into </span>}
+        {/* her icon bir component gibi ekleniyor self closing tag ile */}
+      </div>
+
+      <div
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+        onDoubleClick={handleDoubleClick}
+      >
+        todo item 2 <FaEvernote /> double click here!
+      </div>
+
+      <div 
+      id="todo-3" 
+      className="bg-success text-light w-50 p-4 my-4" 
+      onMouseMove={handleMouseMove} >
+        todo item 3 <FaYinYang />
+      </div>
+
+      <p>X and Y</p>
+      <p className="text-danger fw-bold">
+        {coordX} {coordY}
+      </p>
+    </div>
+  );
+};
+
+export default MouseEvent;
